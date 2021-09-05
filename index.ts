f02511a66e3faf89bac6c7f72d7a459da31a633a
Arthur <82236378+Strike21@users.noreply.github.com>
2021-09-05 14:38:28 -0300

    slash

--- index.ts
+++ index.ts
@@ -2,13 +2,41 @@ import Discord from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config()
 
+
 const client = new Discord.Client({
     intents : 32767
 });
 
 client.on("ready", () => {
     console.log("TS on")
+
+    const ownerGuildId = "831885012890550343"
+    const ownerGuild = client.guilds.cache.get(ownerGuildId)
+    let commands
+
+    if(ownerGuild) {
+        commands = ownerGuild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create ({
+        name:'ping',
+        description : "Latência do bot em relação a api!"
+    })
 });
+
+client.on("interactionCreate", async(interaction) => {
+    if(!interaction.isCommand()) {
+        return
+    }
+
+    const {commandName, options} = interaction
+
+    if(commandName === 'ping') {
+        interaction.reply({ content:`${client.ws.ping}`, ephemeral:true})
+    }
+})
 
 client.on('messageCreate', (message) => {
     if(message.content == 'ping') {
